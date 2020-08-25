1f7ca498c2aef0a4e69dfcc5fe6c9d4088318c62
Codysseus23 <Codysseus23@github.com>
2020-08-25 11:03:20 -0400

    Added nested dict

---
+++ nestdict.py
@@ -0,0 +1,26 @@
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+
+print(ramit["email"])
+print(ramit['interests'][0])
+
+print(ramit['friends'][0]['email'])
+# friends = ramit['friends'][1]
+# friends_interest = friends['interests'][1]
+print(ramit['friends'][1]['interests'][1])
